1716297c2e2bd61d8a0c6918384b496f605adc24
Nathan Hynes <nathanmail@hotmail.co.uk>
2020-01-31 15:28:47 +0000

    Initial Commit

---
+++ iDine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Nathan Hynes on 31/01/2020.
+//  Copyright © 2020 Nathan Hynes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
